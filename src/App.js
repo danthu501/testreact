77713d3cc3475a17a74f6149e506c31a0a4c5590
Daniel Thulin <daniel_thulin@outlook.com>
2022-05-25 10:46:15 +0200

    Added . in app.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ export default function App() {
 return (
     <div className="App">
         <Routes>
-            <Route path="/" element = {<Home/>}/>
-            <Route path="/About/" element = {<About/>}/>
-            <Route path="/Privacy/" element = {<Privacy/>}/>
+            <Route path="./" element = {<Home/>}/>
+            <Route path="./About/" element = {<About/>}/>
+            <Route path="./Privacy/" element = {<Privacy/>}/>
         </Routes>
     </div>
 );
